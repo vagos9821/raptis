942c3f627c0f69a46013eec184dc8dcedee8feb9
Vangelis Sigalas <yourtube12321@gmail.com>
2025-03-13 18:18:34 +0200

    add no index

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,6 +4,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="#">
     <title>{{ __('e-Bags - ΡΑΠΤΗΣ ΛΕΩΝΙΔΑΣ') }}</title>
